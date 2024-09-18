43546a6412bea59ea9b8d50a75c99c313640c83a
pawel.zaborowski2 <pawel.zaborowski@giraffestudio.pl>
2024-09-18 16:03:36 +0200

    import modules

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { TaskEntity } from './task.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([TaskEntity]),],
   controllers: [TaskController],
   providers: [TaskService],
   exports: [TaskService],
